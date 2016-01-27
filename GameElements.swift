b38d5561f31f33c532260b1a7fdf4aab70ca8691
surrus <xsurrus@gmail.com>
2016-01-27 13:44:23 -0700

    gravity

--- GameElements.swift
+++ GameElements.swift
@@ -104,11 +104,36 @@ extension GameScene {
     node.physicsBody = SKPhysicsBody(rectangleOfSize: sprite.size)
     node.physicsBody?.dynamic = false
     node.physicsBody?.categoryBitMask = CollisionBitMask.Brick
-    node.physicsBody?.contactTestBitMask = 0
+    node.physicsBody?.collisionBitMask = 0
     
     return node
-    
   }
 
-
+  func createFlowerAtPosition(position:CGPoint, ofType type:FlowerType) -> FlowerNode {
+    let node = FlowerNode()
+    let position = CGPoint(x: position.x * scaleFactor, y: position.y)
+    node.position = position
+    node.name = "FLOWERNODE"
+    node.flowerType = type
+    
+    var sprite:SKSpriteNode
+    
+    if type == FlowerType.normalFlower {
+      sprite = SKSpriteNode(imageNamed: "flower")
+      
+      
+    }
+    else {
+      sprite = SKSpriteNode(imageNamed: "flowerSpecial")
+    }
+    
+    node.addChild(sprite)
+    
+    node.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width/2)
+    node.physicsBody?.dynamic = false
+    node.physicsBody?.categoryBitMask = CollisionBitMask.Flower
+    node.physicsBody?.collisionBitMask = 0
+    
+    return node
+  }
 }
--- GameScene.swift
+++ GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
   
   var background:SKNode!
   var midground:SKNode!
@@ -64,9 +64,30 @@ class GameScene: SKScene {
     let platform = createPlatformAtPosition(CGPoint(x: 160, y: 320), ofType: PlatformType.normalBrick)
     foreground.addChild(platform)
     
+    let flower = createFlowerAtPosition(CGPoint(x:160, y:220), ofType: FlowerType.specialFlower)
+    foreground.addChild(flower)
+    
+    physicsWorld.gravity = CGVector(dx: 0, dy: -2)
+    physicsWorld.contactDelegate = self
+    
+  }
+  
+  func didBeginContact(contact: SKPhysicsContact) {
+    var otherNode:SKNode!
+    
+    if contact.bodyA.node != player {
+      otherNode = contact.bodyA.node
+    }
+    else {
+      otherNode = contact.bodyB.node
+    }
+    
+    (otherNode as! GenericNode).collisionWithPlayer(player)
   }
   
   override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    player.physicsBody?.dynamic = true
+    player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 20))
     
   }
   
